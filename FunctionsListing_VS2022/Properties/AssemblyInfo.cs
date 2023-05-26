d257d9a4c1cb2cb9424399f2ea384440e90a84c4
aranhil <aranhil@yahoo.com>
2023-05-26 20:31:10 +0300

    fix for 2022

--- FunctionsListing_VS2022/Properties/AssemblyInfo.cs
+++ FunctionsListing_VS2022/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FunctionsListing")]
+[assembly: AssemblyTitle("SearchableNavbar")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FunctionsListing")]
+[assembly: AssemblyProduct("SearchableNavbar")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
